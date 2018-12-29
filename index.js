49c0de9014d11cf20b76370fd3dafe9ca218126e
wenyunyan <wenyunyan@maoyan.com>
2018-12-29 17:58:19 +0800

    添加eslint

--- index.js
+++ index.js
@@ -6,21 +6,20 @@
 
 import thrift from 'thrift';
 
-let server = thrift.createServer(Hello, {
-    say(name, callback){
-      callback(null, 'Hello ' + name);
-    }
-  }, {});
+const server = thrift.createServer(Hello, {
+  say(name, callback) {
+    callback(null, `Hello ${name}`);
+  }
+}, {});
 
 server.listen(7800);
 
 server.on('error', console.error);
 
 server.on('listening', () => {
+  const conn = thrift.createConnection('127.0.0.1', 7800);
+  const client = thrift.createClient(Hello, conn);
 
-  let conn = thrift.createConnection('127.0.0.1', 7800);
-  let client = thrift.createClient(Hello, conn);
-  
   client.say('Thrift', console.log);
   // null 'Hello Thrift'
 });
