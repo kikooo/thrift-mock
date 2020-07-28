6487022f2e3e84e4b39b7b4ff4fa9048b38a5e32
wenyunyan <wenyunyan@maoyan.com>
2020-07-28 16:55:44 +0800

    egg-plugin

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { getMockByMethod } = require('./lib/mock');
+const { getMockTreeNode } = require('./lib/getMockTreeNode');
+
+
+class ThriftMocker {
+  /**
+   * 
+   * @param {Object=} [options={}] 参数
+   * @param {string} options.clientId 应用 ID
+   * @param {string} options.clientSecret 应用 密钥
+   * @param {string} options.callbackUri 登录回调地址
+   */
+  constructor(options = {}) {
+    this.services = options;
+    for (const k in options) {
+      if (Object.prototype.hasOwnProperty.call(config, k) && k !== 'default') {
+        getMockTreeNode(options[k].idl);
+      }
+    }
+  }
+
+  request(serviceName, methodName) {
+    const filePath = this.services[serviceName].idl;
+    return getMockByMethod(filePath, serviceName, methodName)
+  }
+}
+
+module.exports = app => {
+  const config = app.config.thriftMock;
+  app.thriftMocker = new ThriftMocker(config);
+};
