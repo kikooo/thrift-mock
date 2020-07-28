6319fc8d754709d328af42fe4f2fa8eb3c817f97
wenyunyan <wenyunyan@maoyan.com>
2020-07-28 20:20:51 +0800

    eslint

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const { getMockByMethod } = require('./lib/mock');
 const { getMockTreeNode } = require('./lib/getMockTreeNode');
@@ -6,7 +6,7 @@ const { getMockTreeNode } = require('./lib/getMockTreeNode');
 
 class ThriftMocker {
   /**
-   * 
+   *
    * @param {Object=} [options={}] 参数
    * @param {string} options.clientId 应用 ID
    * @param {string} options.clientSecret 应用 密钥
@@ -14,16 +14,14 @@ class ThriftMocker {
    */
   constructor(options = {}) {
     this.services = options;
-    for (const k in options) {
-      if (Object.prototype.hasOwnProperty.call(options, k) && k !== 'default') {
-        getMockTreeNode(options[k].idl);
-      }
-    }
+    Object.keys(options).forEach(k => {
+      getMockTreeNode(options[k].idl);
+    });
   }
 
   request(serviceName, methodName) {
     const filePath = this.services[serviceName].idl;
-    return getMockByMethod(filePath, serviceName, methodName)
+    return getMockByMethod(filePath, serviceName, methodName);
   }
 }
 
